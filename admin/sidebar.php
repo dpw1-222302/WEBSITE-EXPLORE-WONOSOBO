9f1d084502aa5da9e269810678c58a5c7b5e9e0a
Muhammad Ramdani <muhammadramdanibjrs@gmail.com>
2023-07-16 19:55:43 +0700

    Memasukkan template admin

---
+++ admin/sidebar.php
@@ -0,0 +1,28 @@
+<nav class="sb-sidenav accordion sb-sidenav-dark" id="sidenavAccordion">
+    <div class="sb-sidenav-menu">
+        <div class="nav">
+            <div class="sb-sidenav-menu-heading">Core</div>
+            <a class="nav-link" href="index.php">
+                <div class="sb-nav-link-icon"><i class="fas fa-tachometer-alt"></i></div>
+                Dashboard
+            </a>
+
+            <div class="sb-sidenav-menu-heading">Interface</div>
+            <!-- Yang boleh di isi, mulai dari sini -->
+
+            <!-- Sampai sini -->
+
+            <div class="sb-sidenav-menu-heading">User</div>
+
+            <div class="sb-sidenav-menu-heading">Template</div>
+            <a class="nav-link" href="template.php">
+                <div class="sb-nav-link-icon"><i class="fas fa-tachometer-alt"></i></div>
+                Tempate Halaman
+            </a>
+        </div>
+    </div>
+    <div class="sb-sidenav-footer">
+        <div class="small">Logged in as:</div>
+        Admin
+    </div>
+</nav>
